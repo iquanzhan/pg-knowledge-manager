d37ba418a8edcba0126f7367424c109a1833bec0
iquanzhan <iquanzhan@163.com>
2019-01-07 22:29:38 +0800

    init

---
+++ web/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      你好
+      </div>
+    );
+  }
+}
+
+export default App;
