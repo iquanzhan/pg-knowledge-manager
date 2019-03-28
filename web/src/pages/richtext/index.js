ff4bcf70fa5b57925ee6f2db7e87c465d2ba2402
iquanzhan <iquanzhan@163.com>
2019-03-28 17:00:36 +0800

    richtext

--- web/src/pages/richtext/index.js
+++ web/src/pages/richtext/index.js
@@ -1,56 +1,80 @@
-import React, { Component } from 'react';
-import { Input, Button, Row, Col } from 'antd';
-
-// 引入编辑器组件
-import BraftEditor from 'braft-editor'
-// 引入编辑器样式
 import 'braft-editor/dist/index.css'
+import React from 'react'
+import BraftEditor from 'braft-editor'
+import { Form, Input, Button } from 'antd'
 
-class Index extends Component {
+const FormItem = Form.Item;
+class FormDemo extends React.Component {
+    handleSubmit = (event) => {
 
-    state = {
-        // 创建一个空的editorState作为初始值
-        editorState: BraftEditor.createEditorState(null)
-    }
+        this.props.form.validateFields((error, values) => {
+            if (!error) {
+                const submitData = {
+                    title: values.title,
+                    content: values.content.toHTML() // or values.content.toHTML()
+                }
 
-    componentDidMount() {
-
-    }
-
-    submitContent = async () => {
-        // 在编辑器获得焦点时按下ctrl+s会执行此方法
-        // 编辑器内容提交到服务端之前，可直接调用editorState.toHTML()来获取HTML格式的内容
-        const htmlContent = this.state.editorState.toHTML();
-        console.log(htmlContent);
-        //const result = await saveEditorContent(htmlContent)
-    }
-
-    handleEditorChange = (editorState) => {
-        this.setState({ editorState })
+                console.log(submitData)
+            }
+        })
     }
 
     render() {
-        const { editorState } = this.state
+
+        const { getFieldDecorator } = this.props.form;
+        const formItemLayout = {
+            labelCol: {
+                xs: { span: 24 },
+                sm: { span: 2 },
+            },
+            wrapperCol: {
+                xs: { span: 24 },
+                sm: { span: 22 },
+            },
+        };
+
         return (
-            <div className="my-component">
-                <Row>
-                    <Col span={22}>
-                        <Input size="large" placeholder="请输入文章标题" />
-                    </Col>
-                    <Col span={1}>
-                        <Button style={{ marginLeft: 15 }} type="primary" size="large">发布</Button>
-                    </Col>
-                </Row>
-
-                <BraftEditor
-                    value={editorState}
-                    onChange={this.handleEditorChange}
-                    onSave={this.submitContent}
-                />
-
+            <div style={{ paddingTop: 40,width:'90%' }}>
+                <Form onSubmit={this.handleSubmit}>
+                    <FormItem {...formItemLayout} label="文章标题">
+                        {getFieldDecorator('title', {
+                            rules: [{
+                                required: true,
+                                message: '请输入标题',
+                            }],
+                        })(
+                            <Input size="large" placeholder="请输入标题" />
+                        )}
+                    </FormItem>
+                    <FormItem {...formItemLayout} label="文章正文">
+                        {getFieldDecorator('content', {
+                            validateTrigger: 'onBlur',
+                            rules: [{
+                                required: true,
+                                validator: (_, value, callback) => {
+                                    if (value.isEmpty()) {
+                                        callback('请输入正文内容')
+                                    } else {
+                                        callback()
+                                    }
+                                }
+                            }],
+                        })(
+                            <BraftEditor
+                                style={{ height: 450,border:'1px solid #D1D1D1' }}
+                                placeholder="请输入正文内容"
+                            />
+                        )}
+                    </FormItem>
+                    <FormItem {...formItemLayout} label="&nbsp;">
+                        <Button size="large" type="primary" htmlType="submit">提交</Button>
+                    </FormItem>
+                </Form>
             </div>
         )
+
     }
+
 }
 
-export default Index;
+export default Form.create()(FormDemo)
